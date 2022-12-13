f5999d38e832f6b1ae775c8b2128be926ab25fe8
sexypanda3 <spande@caldwell.edu>
2022-12-13 07:31:59 -0500

    k

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 'use strict'
 
 function othername() {
-  const artkotha = ["","CS 301","CS 504", "CS 123", "CS 31","CS 04", "CS 23"];
+  const artkotha = ["","CS 115 001","CS 504", "CS 123", "CS 31","CS 04", "CS 23"];
  
   var input = document.getElementById("userInput").value;  
   var userkotha = jQuery.inArray(input, artkotha);
   var anka = Object.values(artkotha)[userkotha];
  
-  if (anka == "CS 301") {  
-  window.open('albertus.html'); 
+  if (anka = "CS 115 001") {  
+  window.open('albertus115001.html'); 
   }
 
-    else if (anka == "") {
+    else if (anka = "") {
       alert("Please Enter a CS Class Number!"); 
    }
-   
+  
    else {
-    $("h1").remove();
+    alert("Please Enter a CS Class Number!"); 
    }
-   return true;
+   return false;
 }
 
