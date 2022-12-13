2fa7189ecfce3d45fe210777db03f4a1c4206c5e
sexypanda3 <spande@caldwell.edu>
2022-12-12 23:37:07 -0500

    1

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function othername() {
+  const artkotha = ["","CS 301","CS 504", "CS 123", "CS 31","CS 04", "CS 23"];
+ 
+  var input = document.getElementById("userInput").value;  
+  var userkotha = jQuery.inArray(input, artkotha);
+  var anka = Object.values(artkotha)[userkotha];
+ 
+  if (anka == "CS 301") {  
+  window.open('albertus.html'); 
+  }
+    else if (anka == "") {
+      alert("Please Enter a CS Class Number!"); 
+   }
+   else {
+    alert("Please Enter a CS Class Number!"); 
+   }
+   return false;
+}
+
