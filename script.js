c1709123f1020e50d8912674c5e350c01c24a61f
sexypanda3 <spande@caldwell.edu>
2022-12-13 19:01:35 -0500

    1

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 'use strict'
 
-function othername() {
-  const artkotha = ["","CS 115 001","CS 504", "CS 123", "CS 31","CS 04", "CS 23"];
+function haita() {
+  const artkotha = ["CS 196 003","CS 196 002","CS 115 001","CS 115 002", "CS 115 500", "CS 195 001","CS 195 002", "CS 196 001"];
  
   var input = document.getElementById("userInput").value;  
   var userkotha = jQuery.inArray(input, artkotha);
   var anka = Object.values(artkotha)[userkotha];
- 
-  if (anka = "CS 115 001") {  
-  window.open('albertus115001.html'); 
-  }
 
-    else if (anka == "") {
-      alert("Please Enter a CS Class Number!"); 
+    if (anka === "CS 115 001") {  
+    window.open('albertus115001.html'); 
+    }
+    if (anka === "CS 115 002") {  
+    window.open('albertus115002.html'); 
+    }
+    if (anka === "CS 115 500") {  
+      window.open('albertus115500.html'); 
+    }
+    if (anka === "CS 195 001") {  
+      window.open('albertus195001.html'); 
+      }
+      if (anka === "CS 195 002") {  
+      window.open('albertus195002.html'); 
+      }
+      if (anka === "CS 196 001") {  
+        window.open('albertus196001.html'); 
+      }
+      if (anka === "CS 196 002") {  
+        window.open('albertus196002.html'); 
+      }
+      if (anka === "CS 196 003") {  
+        window.open('albertus196003.html'); 
+      }
+    else if (anka === "") {
+      alert("Please Enter a Class Number!"); 
    }
-  
+
    else {
     alert("Please Enter a CS Class Number!"); 
    }
    return false;
-}
-
+  }
